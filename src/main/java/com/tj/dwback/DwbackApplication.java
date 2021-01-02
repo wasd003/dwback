c1f5e532a8215613331b526edc358c2052b8394d
wasd003 <2206475938@qq.com>
2021-01-02 20:14:31 +0800

    初始化

---
+++ src/main/java/com/tj/dwback/DwbackApplication.java
@@ -0,0 +1,13 @@
+package com.tj.dwback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tj/dwback/DwbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tj.dwback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
